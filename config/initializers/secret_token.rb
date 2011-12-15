3a66d276d4acd28c71be88b1f3b5403e7e403171
Erwin Boskma <e.boskma@datarift.nl>
2011-12-16 00:13:00 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OakPm::Application.config.secret_token = 'b612958d0c5173fb9a2d2a2af83c0b08bfaca6856805282045ca58e3d500ec13c20f75ba09ae85370a6fc91315189e1b7f94b9f0adfe08f256ede8ae2535549a'
