4fb5bb5f5d1231ac433b160f3afab18dfe1861d5
Erwin Boskma <e.boskma@datarift.nl>
2011-12-16 12:16:06 +0100

    * Installed RSpec * Converted layout file to HAML * Installed Nifty Layout

--- config/application.rb
+++ config/application.rb
@@ -50,5 +50,9 @@ module OakPm
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.generators do |g|
+      g.template_engine :haml
+    end
   end
 end
